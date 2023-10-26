6d020bcaeadc1df2b0b0288e4841f0e6265d33fd
jonathancastillo185 <jonathancastillo185@gmail.com>
2023-10-26 15:42:52 -0300

    proceso

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = FastAPI()
 
 
 df = pd.read_csv(r'data_set_limpio/games_preparado.csv.gz')
-item = pd.read_csv(r'data_set_limpio/items_preparado.csv.gz')
-opinion = pd.read_csv(r'data_set_limpio/reviews_preparado.csv.gz')
+
+#item = pd.read_csv(r'data_set_limpio/items_preparado.csv.gz')
+
+#opinion = pd.read_csv(r'data_set_limpio/reviews_preparado.csv.gz')
 
 
 
